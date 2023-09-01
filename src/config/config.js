3573bdb3fccde7ba35e1cfab8b569eba4fccaa43
Bernardo <bebezanetti@gmail.com>
2023-08-31 21:08:47 -0300

    feat: criando crus

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,13 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 import 'dotenv/config';
 
 export const sequelize = new Sequelize(
-    process.env.POSTGRES_DB,
-    process.env.POSTGRES_USERNAME,
-    process.env.POSTGRES_PASSWORD,
-    {
-        host: process.env.POSTGRES_HOST,
-        dialect: 'postgres'
-    }
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USERNAME,
+  process.env.POSTGRES_PASSWORD,
+  {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    dialect: 'postgres',
+  },
 );
