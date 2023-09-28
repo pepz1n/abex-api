a06a02437d9a91b3d6d8b5f987cd3fa93217ac2d
Bernardo <Bernardo.zanetti@unochapeco.edu.br>
2023-09-27 21:03:29 -0300

    Revert "Feat/crud denuncias permission"

--- src/routes/contactRoute.js
+++
@@ -1,9 +0,0 @@
-import controller from '../controllers/contactController';
-
-export default (app) => {
-  app.post('/contact/persist', controller.persist);
-  app.post('/contact/persist/:id', controller.persist);
-  app.post('/contact/destroy', controller.destroy);
-  app.get('/contact', controller.get);
-  app.get('/contact/:id', controller.get);
-};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,9 @@
 import adressRoute from './addressRoute';
 import institutionRoute from './institutionRoute';
-import contactRoute from './contactRoute';
-import personAccountRoute from './personAccountRoute';
-import { removeAllListeners } from 'nodemon';
 
 function Routes(app) {
   institutionRoute(app)
   adressRoute(app);
-  contactRoute(app);
-  personAccountRoute(app);
 }
 
 export default Routes;
