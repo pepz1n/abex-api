ee66a84cfac9e0600621cc0c2047f19072837355
Bernardo <Bernardo.zanetti@unochapeco.edu.br>
2025-08-08 16:53:48 -0300

    Add basic frontend and root route

--- src/server.js
+++ src/server.js
@@ -31,6 +31,10 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use('/public', express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 routes(app);
 app.use((req, res) => {
   res.status(404).send('404 - Página não encontrada');
